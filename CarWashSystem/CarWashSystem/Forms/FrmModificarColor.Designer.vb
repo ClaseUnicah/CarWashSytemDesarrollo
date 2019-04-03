25141be28c0ea32a783555328ab03ada156c94de
Oscar Omar Pineda <oscarpineda05@gmail.com>
2019-04-02 18:50:07 -0600

    Formulario Editar Color

---
+++ CarWashSystem/CarWashSystem/Forms/FrmModificarColor.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmModificarColor
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.BtnCancelar = New System.Windows.Forms.Button()
+        Me.BtnAplicar = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TxtNombreColor = New System.Windows.Forms.TextBox()
+        Me.TxtIdColor = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.EpMensaje = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.EpMensaje, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BtnCancelar
+        '
+        Me.BtnCancelar.Location = New System.Drawing.Point(314, 204)
+        Me.BtnCancelar.Name = "BtnCancelar"
+        Me.BtnCancelar.Size = New System.Drawing.Size(95, 41)
+        Me.BtnCancelar.TabIndex = 5
+        Me.BtnCancelar.Text = "Cancelar"
+        Me.BtnCancelar.UseVisualStyleBackColor = True
+        '
+        'BtnAplicar
+        '
+        Me.BtnAplicar.Location = New System.Drawing.Point(200, 204)
+        Me.BtnAplicar.Name = "BtnAplicar"
+        Me.BtnAplicar.Size = New System.Drawing.Size(108, 41)
+        Me.BtnAplicar.TabIndex = 4
+        Me.BtnAplicar.Text = "Aplicar"
+        Me.BtnAplicar.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TxtNombreColor)
+        Me.GroupBox1.Controls.Add(Me.TxtIdColor)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(21, 32)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(568, 157)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        '
+        'TxtNombreColor
+        '
+        Me.TxtNombreColor.Location = New System.Drawing.Point(184, 97)
+        Me.TxtNombreColor.Multiline = True
+        Me.TxtNombreColor.Name = "TxtNombreColor"
+        Me.TxtNombreColor.Size = New System.Drawing.Size(321, 37)
+        Me.TxtNombreColor.TabIndex = 3
+        '
+        'TxtIdColor
+        '
+        Me.TxtIdColor.Location = New System.Drawing.Point(184, 31)
+        Me.TxtIdColor.Multiline = True
+        Me.TxtIdColor.Name = "TxtIdColor"
+        Me.TxtIdColor.ReadOnly = True
+        Me.TxtIdColor.Size = New System.Drawing.Size(163, 34)
+        Me.TxtIdColor.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(51, 103)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(95, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nombre Color"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(51, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Id Color"
+        '
+        'EpMensaje
+        '
+        Me.EpMensaje.ContainerControl = Me
+        '
+        'FrmModificarColor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(633, 271)
+        Me.Controls.Add(Me.BtnCancelar)
+        Me.Controls.Add(Me.BtnAplicar)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "FrmModificarColor"
+        Me.Text = "FrmModificarColor"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.EpMensaje, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BtnCancelar As Button
+    Friend WithEvents BtnAplicar As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TxtNombreColor As TextBox
+    Friend WithEvents TxtIdColor As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents EpMensaje As ErrorProvider
+End Class
---
+++ CarWashSystem/CarWashSystem/Forms/FrmModificarColor.vb
@@ -0,0 +1,63 @@
+﻿Imports System.Data.SqlClient
+Public Class FrmModificarColor
+    Private Sub BtnAplicar_Click(sender As Object, e As EventArgs) Handles BtnAplicar.Click
+        If ValidarTextBox() = True Then
+            ActualizarColor()
+            Limpiar()
+        End If
+
+    End Sub
+
+    Private Sub Limpiar()
+        TxtIdColor.Text = Nothing
+        TxtNombreColor.Text = Nothing
+
+    End Sub
+
+    Private Function ValidarTextBox() As Boolean
+
+        Dim Estado As Boolean
+
+        If TxtNombreColor.Text = Nothing Then
+            EpMensaje.SetError(TxtNombreColor, "Tiene que ingrear el nombre del color.")
+            TxtNombreColor.BackColor = Color.LightBlue
+            TxtNombreColor.Focus()
+            Estado = False
+        Else
+            Estado = True
+            EpMensaje.SetError(TxtNombreColor, "")
+        End If
+        Return Estado
+
+    End Function
+
+    Private Sub ActualizarColor()
+        If Cn.State = ConnectionState.Open Then
+            Cn.Close()
+        End If
+
+        Try
+            Cn.Open()
+            Using Cmd As New SqlCommand
+                With Cmd
+                    .CommandText = "Sp_ModificarColor"
+                    .CommandType = CommandType.StoredProcedure
+                    .Connection = Cn
+
+                    ' Parámetros
+                    .Parameters.Add("@NombreColor", SqlDbType.NVarChar, 20).Value = TxtNombreColor.Text
+                    .Parameters.Add("@IdColor", SqlDbType.Int).Value = CInt(TxtIdColor.Text)
+                    .ExecuteNonQuery()
+
+                    MessageBox.Show("Registro modificado satisfactoriamente", "CarWashSystem", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End With
+
+
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error al almacenar el registro" + ex.Message, "CarWashSystem", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            Cn.Close()
+        End Try
+    End Sub
+End Class
